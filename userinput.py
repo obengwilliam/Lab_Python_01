42e41b759b159fedf91ad4fd2f4eca121ed9c372
AITI Student <aiti-student@aiti-090-PC.(none)>
2012-06-20 07:38:40 -0400

    just finished the work

---
+++ userinput.py
@@ -0,0 +1,11 @@
+'''
+creator:obeng william
+description:USER INPUT NAME ,DATE OF BIRTH
+'''
+firstname=raw_input("Enter your first name:")
+lastname=raw_input("Enter your last name:")
+print "Enter your date of birth"
+month=raw_input("       Month?")
+day=raw_input("         Day?")
+year=raw_input("        Year?")
+print firstname.upper(),lastname.upper(),"was born on",month,day,year
---
+++ zellers.py
@@ -0,0 +1,33 @@
+'''
+creator:obeng william
+description:Zellers alogorithm
+
+'''
+
+firstname=raw_input("Enter your first name:")
+lastname=raw_input("Enter your last name:")
+print "Enter your date of birth"
+print "Enter march as 1 and april as 2 ----december as 10"
+print "IF BORN IN JANUARY OR FEBRUARY THEN ENTER 11 FOR JANUARY AND 12 FOR FEBRUARY "
+month=raw_input("       Month as a number betweeen 1-12 ?")
+day=raw_input("         Day?")
+year=raw_input("        Year?")
+print firstname.upper(),lastname.upper(),"was born on",month,day,year
+
+a=int(month) #month born
+b=int(day )#day of month
+c=int(year[2:])
+print c
+d=int(year[:2])
+
+if a==11:
+    c=c-1
+elif a==12:
+    c=c-1
+    
+w=(13 *a-1)/5
+x=c/4
+y=d/4
+z=w+x+y+b+c-2*d
+r=z%7
+print "THis is the day in which you were born",r
